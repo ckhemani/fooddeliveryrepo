c44a307df89a626111b7c98c3a4575635555168c
ckhemani <chandru.khemani@gmail.com>
2022-04-03 19:23:38 -0400

    added Getters and Setters for Driver

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -8,5 +8,21 @@ public class Driver {
         this.name = name;
         this.car = car;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
 }
 
